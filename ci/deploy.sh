7c19aa2c95dcb9c71721f49fa22ec14b0f680b68
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-12-11 22:03:29 +0200

    Don't push artifacts to remote location and generate output

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,4 +5,8 @@ set -e -u
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
 cd java-buildpack-metric-writer
-./mvnw -q -Dmaven.test.skip=true deploy
+./mvnw -q -Dmaven.test.skip=true package
+
+mkdir -p ../output
+cp $(ls target/java-buildpack-metric-writer-*.jar | grep -v sources) ../output/
+echo $(cat target/maven-archiver/pom.properties  | grep version= | cut -d= -f2) > ../output/version
